7b16345297e0b39912a164b8d554431ec761f7eb
Kim-YH-handong <82792998+Kim-YH-handong@users.noreply.github.com>
2021-12-10 17:24:22 +0900

    Login Complete

--- src/main/java/com/app/app/FuneralController.java
+++ src/main/java/com/app/app/FuneralController.java
@@ -1,8 +1,44 @@
 package com.app.app;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.app.app.service.UserServiceImpl;
+import com.app.app.vo.DeadVO;
+import com.app.app.vo.UserVO;
 
 @Controller
 public class FuneralController {
+	
+	@Autowired
+	UserServiceImpl service;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login/loginOk", method = RequestMethod.POST)
+	public String loginOk(HttpSession session, UserVO vo) {
+		System.out.println(vo.getUserid());
+		String returnURL = "";
+		if (session.getAttribute("login") != null) {
+			session.removeAttribute("login");
+		}
+		UserVO uservo = service.getUser(vo);
+		if (uservo != null) { // 로그인 성공
+			System.out.println("로그인 성공!");
+			session.setAttribute("user", uservo);
+			returnURL = "redirect:/code";
+		} else { // 로그인 실패
+			System.out.println("로그인 실패!");
+			returnURL = "redirect:/login";
+		}
+		return returnURL;
+	}
 
 }
--- src/main/java/com/app/app/service/UserServiceImpl.java
+++ src/main/java/com/app/app/service/UserServiceImpl.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.app.dao.UserDAO;
+import com.app.app.vo.DeadVO;
+import com.app.app.vo.UserVO;
 
 @Service
 public class UserServiceImpl {
 	
 	@Autowired
 	UserDAO userDAO;
-
+	
+	public UserVO getUser(UserVO vo) {
+		return userDAO.getUser(vo);
+	}
 }
